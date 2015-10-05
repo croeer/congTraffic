1261d902cb9cabf3e099daf2b04e4ab51a38e1ba
croeer <christian@roeer.info>
2015-10-05 19:56:23 +0200

    Initial Commit

---
+++ congTraffic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  congTraffic
+//
+//  Created by Christian Röer on 05.10.15.
+//  Copyright © 2015 Christian Röer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
